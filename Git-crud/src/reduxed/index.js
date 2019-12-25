b89121ce3e6b5ff40cc87f827de1051a631da96b
Vijay Dubey <vijayd@chetu.com>
2019-12-25 22:36:14 +0530

    adding redux for crud

--- Git-crud/src/reduxed/index.js
+++ Git-crud/src/reduxed/index.js
@@ -1,50 +1,68 @@
 import React, { Component } from 'react';
-//https://levelup.gitconnected.com/react-crud-app-without-and-with-redux-da4cd87f2eab
-//https://reqres.in/
+import logo from '../logo.svg';
+import '../App.css';
+import StudentList from './StudentList.jsx';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addStudent, deleteStudent, updateStudent } from './actions/studentActions';
 
-import Store from '../store.js'
+class index extends Component {
+    constructor(props) {
+        super(props);
+        this.addNewStudent = this.addNewStudent.bind(this);
+        this.deleteStudent = this.deleteStudent.bind(this);
+        this.editStudentSubmit = this.editStudentSubmit.bind(this);
+    }
 
-class reduxed extends Component {
-constructor(){
-	super();
 
-		this.state = [
-            { name: "vijay", Email: "vijayd@gmail.com", Created: "" },
-            { name:"Arun", Email: "Arun@gmail.com", Created: "" },
-          { name: "vinay", Email: "vinay@gmail.com", Created: "" }
-        ];
-}
+    addNewStudent() {
+        this.props.addStudent({ id: Math.max(...this.props.studentList.map(function (o) { return o.id })) + 1, name: '', grade: 1, school: '' });
+    }
 
-render() {
+    deleteStudent(id) {
+        let r = window.confirm("Do you want to delete this item");
+        if (r === true) {
+            this.props.deleteStudent(id);
+
+        }
+    }
+
+    editStudentSubmit(id, name, grade, school) {
+        this.props.updateStudent({ id: id, name: name, grade: grade, school: school });
+    }
+
+    render() {
         return (
-            <div>
-                  <p>{(this.state.map((a)=> a.name))}</p>
-
-
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Grade</th>
-                            <th>School</th>
-                          
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr >
-                            <td>1</td>
-                            <td>2</td>
-                            <td>
-                                <button className="button muted-button"> Edit </button>
-                                <button className="button muted-button">Delete</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        )
+            <div className="container-fluid">
+                <div className="row mt-3"><div className="col-lg-12">
+                    <div className="card">
+                        <div className="card-header">
+                            Student Registry
+  </div>
+                        <div className="card-body">
+                            <table className="table table-hover">
+                                <thead className="thead-dark"><tr><th>Name</th><th>Grade</th><th>School</th><th>Edit/Save</th><th>Delete</th></tr></thead>
+                                <StudentList deleteStudent={this.deleteStudent} studentList={this.props.studentList} editStudentSubmit={this.editStudentSubmit} />
+                            </table>
+                            <button className="btn btn-dark pull-left" onClick={this.addNewStudent}>Add New</button>
+                        </div></div>
+                </div></div></div>
+        );
     }
 }
 
-export default reduxed
+const mapStateToProps = (state) => {
+    return {
+        studentList: state
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        addStudent: addStudent,
+        deleteStudent: deleteStudent,
+        updateStudent: updateStudent
+    }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(index);
--- crud/src/Menu/Router.js
+++ crud/src/Menu/Router.js
@@ -4,23 +4,25 @@ import Menu from '../Menu/menu.js'
 import Ecommerce from '../ecommerce/Parent.js'
 import Reduxed from '../reduxed/index.js'
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from '../reduxed/store/store.js';
 
 class Rout extends Component {
 
-render() {
+    render() {
 
         return (
             <div>
-
-                <Router>
-                    <div>
-                        <Route path="/" component={Menu} />
-                        <Route path="/ecommerce" component={Ecommerce} />
-                        <Route path="/Crud" component={Crud} />
-                        <Route path="/redux-crud" component={Reduxed} />
-                    </div>
-                </Router>
-
+                <Provider store={store}>
+                    <Router>
+                        <div>
+                            <Route path="/" component={Menu} />
+                            <Route path="/ecommerce" component={Ecommerce} />
+                            <Route path="/Crud" component={Crud} />
+                            <Route path="/redux-crud" component={Reduxed} />
+                        </div>
+                    </Router>
+                </Provider>
             </div>
         )
 
